697a0311a678e713b11576625ed27e56eef0761a
DauHuy <91195338+DauHuy@users.noreply.github.com>
2022-11-02 13:30:09 +0700

    Add files via upload

---
+++ ex1/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import sounds from './sounds';
+import './styles.css';
+
+function App() {
+  return <div />;
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
---
+++ ex1/src/sounds.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    keyCode: 65,
+    label: 'clap',
+    file: 'sounds/clap.wav',
+  },
+  {
+    keyCode: 83,
+    label: 'hihat',
+    file: 'sounds/hihat.wav',
+  },
+  {
+    keyCode: 68,
+    label: 'kick',
+    file: 'sounds/kick.wav',
+  },
+  {
+    keyCode: 70,
+    label: 'openhat',
+    file: 'sounds/openhat.wav',
+  },
+  {
+    keyCode: 71,
+    label: 'boom',
+    file: 'sounds/boom.wav',
+  },
+  {
+    keyCode: 72,
+    label: 'ride',
+    file: 'sounds/ride.wav',
+  },
+  {
+    keyCode: 74,
+    label: 'snare',
+    file: 'sounds/snare.wav',
+  },
+  {
+    keyCode: 75,
+    label: 'tom',
+    file: 'sounds/tom.wav',
+  },
+  {
+    keyCode: 76,
+    label: 'tink',
+    file: 'sounds/tink.wav',
+  },
+];
